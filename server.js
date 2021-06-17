55c0608e4dd80f5f6406db90ecf437a0dfc05153
facualvez54 <facualvez54@gmail.com>
2021-06-17 15:38:35 -0300

    MemeApp v8

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/meme-app'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/meme-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/meme-app/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/meme-app/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
